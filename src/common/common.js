8d7145adf22da6ba30724f3a891fb2cf9fc6827d
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-09-18 02:26:45 +0900

    [feature/coupong-5] WebSocket 기본 UI 및 연동

--- src/common/common.js
+++ src/common/common.js
@@ -1,2 +1,3 @@
-export const DOMAIN = "https://urecacoupon.store";
+// export const DOMAIN = "https://urecacoupon.store";
+export const DOMAIN = "http://localhost:4040";
 export const API_DOMAIN = `${DOMAIN}/api/v1`;
